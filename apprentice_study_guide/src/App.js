3304ea8402435b950209b86346d6e72b9890ebc8
Josh Jenkins <joshua.jenkins@live.co.uk>
2019-07-31 11:56:57 +0100

    Inital commit of redux-saga overhaul

--- apprentice_study_guide/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import * as Questions_List from "./assets/questions.json";
-import Questions from "./components/Questions/Questions";
-
-function App() {
-  return <Questions questions={Questions_List} />;
-}
-
-export default App;
---
+++ apprentice_study_guide/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../../assets/logo.svg";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Hello there</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
